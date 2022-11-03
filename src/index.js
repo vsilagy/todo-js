df1db198d0509120adf0059a597d973bf5687608
vsilagy <vladsilagy@gmail.com>
2022-11-02 22:11:51 -0400

    add todos to list working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./style.css";
+import './style.css';
+const form = document.getElementById('form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+form.addEventListener('submit', addToto);
+
+function addToto(event) {
+	if (todoInput.value === '') return;
+
+	const li = document.createElement('li');
+	li.className = 'li-style';
+	li.textContent = todoInput.value;
+	todoList.appendChild(li);
+	form.reset();
+	event.preventDefault();
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  content: ["./dist/*.html"],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./dist/*.html'],
+	theme: {
+		extend: {
+			colors: {
+				brightBlue: 'hsl(220, 98%, 61%)',
+				checkBackground:
+					'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+				veryLightGray: 'hsl(0, 0%, 98%)',
+				veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+				lightGrayishBlue: 'hsl(233, 11%, 84%)',
+				darkGrayishBlue: 'hsl(236, 9%, 61%)',
+				veryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+				veryDarkBlue: 'hsl(235, 21%, 11%)',
+				veryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+				lightGrayishBlue: 'hsl(234, 39%, 85%)',
+				lightGrayishBlueHover: 'hsl(236, 33%, 92%)',
+				darkGrayishBlue: 'hsl(234, 11%, 52%)',
+			},
+			fontFamily: {
+				sans: ['Josefin Sans', 'sans-serif'],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
